d5641c4a077e2abe0b86e54030b90302d2925938
ayocodess <ayo.daniel.adesanya@gmail.com>
2023-07-30 21:42:17 +0200

    updated about section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -10,7 +10,7 @@ import {
   LinkedInIcon,
   TwitterIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/ayo.jpeg'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
@@ -61,46 +61,38 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’mAyo Adesanya. I live in New York City, where I design the
-              future.
+              I’m Ayo Adesanya. I live in Sweden, where I help automate internal
+              business processes.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                ’m a software developer who can work across the whole stack with
+                most of my professional experience being in the front-end.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                Interests: tech digitalisation, system integrations,
+                sustainability, love and relationships and Japanese anime. I
+                thrive in teams, helping others grow and building culture that
+                fosters genuine collaboration and connection.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                Working at WP Tech (Sweden) I lead the front-end development and
+                tooling of our cross-platform applications.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                My Keywords: #collectiveAccountability #climateJustice
+                #socialJustice #techforgood
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
-              <SocialLink href="#" icon={TwitterIcon}>
+              {/* <SocialLink href="#" icon={TwitterIcon}>
                 Follow on Twitter
               </SocialLink>
               <SocialLink href="#" icon={InstagramIcon} className="mt-4">
                 Follow on Instagram
-              </SocialLink>
+              </SocialLink> */}
               <SocialLink href="#" icon={GitHubIcon} className="mt-4">
                 Follow on GitHub
               </SocialLink>
@@ -108,11 +100,11 @@ export default function About() {
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:spencer@planetaria.tech"
+                href="mailto:ayo@ayoadesanya.com"
                 icon={MailIcon}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                spencer@planetaria.tech
+                ayo@ayoadesanya.com
               </SocialLink>
             </ul>
           </div>
