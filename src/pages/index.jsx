83ba8ba2b45023f542e05172d00c4b84f9f1992e
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-12-18 14:14:33 +0100

    home page update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -320,7 +320,7 @@ export default function Home({ articles }) {
             workflows.
           </p>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I no longer accept freelance work, but you can still email me{' '}
+            Want to talk?, you can email me{' '}
             <span className="text-blue-500 hover:text-blue-600">
               <Mailto
                 email="ayo@ayoadesanya.com"
