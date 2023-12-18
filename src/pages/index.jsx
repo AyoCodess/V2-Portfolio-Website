fb20c38c925c6776000b78445c0d7f755f1acda8
“ayocodess” <ayo.daniel.adesnya@gmail.com>
2023-12-18 14:00:59 +0100

    text update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -309,8 +309,8 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Ayo Adesanya - Full-stack Developer, People Connector & Stellar team
-            player
+            Ayo Adesanya - Full-stack Developer, People Connector & Stellar Team
+            Player
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I’m Ayo, a skilled full-stack typescript developer specializing in
