f779168be5c26071e3eb110c170be840a0b92fec
ayocodess <ayo.daniel.adesanya@gmail.com>
2024-01-08 16:38:41 +0100

    updated project page

--- src/pages/wp-appen-project.jsx
+++ src/pages/wp-appen-project.jsx
@@ -59,38 +59,97 @@ export default function Projects() {
         title="Elevate your organizations work efficiency"
         intro="WP Appen, a WP Tech internal employee management application.  Designed for both employees and employers, a platform that allows you to manage your work and employees in one place."
       >
-        <article>
-          <section>
-            <p className="mt-6 text-base  text-black dark:text-white"></p>
+        <article className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
+          <h2 className="mt-4 text-2xl font-bold text-white">
+            My Contribution
+          </h2>
+          <p className="mt-6 text-lg text-zinc-600 dark:text-zinc-400">
+            I lead the front-end development, implemented ci/cd, wrote
+            documentation and contributed to the API layer.
+          </p>
+          <h2 className="mt-4 text-2xl font-bold text-white">
+            Time Log Feature
+          </h2>
+          <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+            <b>Time log </b>enables the ability to track when field workers
+            check in and out, allowing for accurate salary calculations. It also
+            tracks the location of the employee when they sign in and out. The
+            feature provides the total reported time for the employee, showing
+            how many hours they’ve worked that day and their current session.
+          </p>
 
-            <section className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-              <p className="mb-6 mt-10 text-xl font-bold text-black dark:text-white">
-                Time Log Feature
-              </p>
-              <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
-                Time-log is a mobile application feature that enables
-                organizations to automate and efficiently manage and prepare
-                employee salary accounting, monitor overtime, and handle absence
-                and internal issue reporting. The following videos below i talk
-                about a 2 core features and my role in the project.
-              </p>
-              <div className="mt-6">
-                <LazyYoutube videoId="eIbGL6ETWXM" />
-              </div>
-              <div className="mt-6">
-                <p className="mb-6 mt-10 text-xl font-bold text-black dark:text-white">
-                  Report Availability Feature
-                </p>
-                <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
-                  An automated system for organizations to gather monthly
-                  schedules from their on-hours staff within a few clicks
-                </p>
-                <div className="mt-6">
-                  <LazyYoutube videoId="ZuUezwzy-xE" />
-                </div>
-              </div>
-            </section>
-          </section>
+          <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+            <b>Time overview:</b> Groups all of the field workers’ sessions by
+            today, yesterday, this week, this month, and last month. It provides
+            a clear breakdown of how the field worker has worked. Managers can
+            also see the same data on their dashboard.
+          </p>
+
+          <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+            <b>Notify absence: </b>If a field worker is sick, they can notify
+            their absence through the app. They have an hour to cancel their
+            sick leave if they decide they can work.
+          </p>
+
+          <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+            <b>Employee Status:</b> If you’re an admin, you can see the employee
+            status, which shows which field workers are checked in, when and
+            where they checked in, and the last time they checked in. This is
+            important for managers to keep track of their staff.
+          </p>
+
+          <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+            Watch the video below to see the features in action and more.
+          </p>
+
+          <div className="mt-6">
+            <LazyYoutube videoId="eIbGL6ETWXM" />
+          </div>
+
+          <div className="mt-6">
+            <p className="mb-6 mt-10 text-xl font-bold text-black dark:text-white">
+              Report Availability Feature
+            </p>
+            <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+              An automated system for organizations to gather monthly schedules
+              from their on-hours staff within a few clicks
+            </p>
+
+            <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+              <b>Report availability:</b> Allows managers to send a monthly
+              schedule to field workers. The schedule starts on the 20th of a
+              given month and ends on the 19th of the next month. Field workers
+              can set their availability for dates outside a 7-day grace period.
+            </p>
+
+            <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+              Field workers can select their available dates and times. They can
+              also select all dates at once or change their availability for
+              individual dates. If they want to change the time for a date, they
+              can do so by clicking the edit button.
+            </p>
+
+            <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+              Once a field worker is happy with their schedule, They can then
+              report their availability and all dates without a selected
+              availability will be marked as unavailable. They can always make
+              changes to individual dates, provided it does not fall within the
+              7-day time grace period.
+            </p>
+
+            <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+              <b>Challenges:</b> Grouping weeks by swedish standards, the data
+              structure sent from the Lime Api not being ideal for the feature.
+            </p>
+
+            <p className="mt-6 text-base  text-zinc-600 dark:text-zinc-400">
+              Watch the video below to see the features in action and more.
+            </p>
+
+            <div className="mt-6">
+              <LazyYoutube videoId="ZuUezwzy-xE" />
+            </div>
+          </div>
         </article>
       </SimpleLayout>
     </>
